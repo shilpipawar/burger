2d692b92af37e28f22b02871bde512c72b45d3b2
shilpipawar <shilpipawar@gmail.com>
2019-01-06 19:28:18 -0600

    Change

--- controllers/burger_Controller.js
+++ controllers/burger_Controller.js
@@ -18,11 +18,11 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
   burger.create([
-    "name", "sleepy"
+    "burger_name", "devoured"
   ], [
-      req.body.name, req.body.sleepy
+      req.body.name, req.body.devoured
     ], function (result) {
-      // Send back the ID of the new quote
+      // Send back the ID of the new burger
       res.json({ id: result.insertId });
     });
 });
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function (req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
